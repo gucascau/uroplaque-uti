da0523cc3a8def2d9b59fe3800fefa4ab66ccd61
gucascau <xinwangcau@gmail.com>
2026-06-19 09:38:23 -0400

    please create a github for the UPK1 publication

---
+++ README.md
@@ -0,0 +1,28 @@
+# Urothelium Heterogeneity in the Urinary System 
+
+## Introduction
+The urinary system works as body's filteration system, which consists of two kidneys, two ureters, a bladder, urethra, and urine. The system can help the body to eliminate liquid waste and keep chemical in balance. Urothelium is a high specialized layer of strtifed epithelia cells in patterning the urinary tract. And it performed as the frontline and multi-faceted barrier against a harsh environment and lining the inner surfiace of other cells. The urothelium is mostly made up of cell types, basal, intermediate and umbresll cells in bladder, however, the accurate specificity and components in other urinary system are stil unknown. Here we collected a total of 291,852 normal human cells from urinary systems, including 184,280 kidney, 54,103 ureter, 33,495 bladder and 19,974 urine cells. After filteration, 204536 cells from 33488 bladder, 99255 kidney and 53438 from Ureter and 18355 from Urine.
+![UrotheliuminUrinaryTractReadMe](https://github.com/gucascau/Urothelium/assets/23031126/003f2c9b-3110-4d04-a656-6f47be26261c)
+
+## Description
+In this project, we have collected the published avaiable single cells in human and mouse urinary system, including kidney, ureter, bladder and urine cells. 
+
+## Datasets
+1. Human single cells in the urinary system. 
+	-  **Kidney**: 13 single cell datasets from healthy controls, including GSM382393, GSM3823940, GSM3823941, GSM4008619, GSM4008620, GSM4008621, GSM4008622, GSM4572192, GSM4572193, GSM4572194, GSM4572195, GSM4572196, and GSM5837792.  
+	-  **Ureter**: 4 single cells from two healthy control, including GSE184111 (1), and GSM4008665 (2)  
+	-  **Bladder**: 5 single cells from 3 patients health bladder and two adult bladder samples, including GSM3723357, GSM3723358, GSM3723359, GSM3980126, and GSM3980127.
+	-  **Urine**: 4 single cells from one health urine and 3 covid urine, including GSM5032756, GSM5706858, GSM5706859, GSM5706860
+
+2. 
+
+## Methods
+
+
+
+## Copyright
+For more detail information, please feel free to contact: xin.wang@nationwidechildrens.org
+
+Copyright (c) 2023 Xin Wang
+
+Current version v1.0
---
+++ scripts/01_preprocessing/Sample_9_GSL-AJ-3279_RNAseq.sh
@@ -0,0 +1,152 @@
+#!/bin/sh
+#SBATCH --time=48:00:00
+#SBATCH --partition=himem
+#SBATCH --cpus-per-task=12
+#SBATCH --job-name=LargeD
+#SBATCH --output=slurm_%j.out
+set -e
+
+
+ml GCC/7.3.0-2.30
+ml OpenMPI/3.1.1
+ml GCCcore/7.3.0
+ml Python/2.7.15
+ml Perl/5.28.0
+#ml  GCC/7.3.0-2.30
+
+
+module load TopHat/2.1.2
+module load Cufflinks/2.2.1
+module load  SAMtools/1.15
+module load Trim_Galore/0.6.0-Python-2.7.15
+module load  HTSeq/0.12.4
+module load Bowtie2/2.3.4.2
+
+
+
+### set up the enviroment (GRCm38.p6)
+mm10=/home/gdbecknelllab/xxw004/Projects/GenomeIndex/Mus_musculus/UCSC/latest/mm10
+gtf=/home/gdbecknelllab/xxw004/Projects/GenomeIndex/Mus_musculus/UCSC/genes/mm10.ensGene.gtf
+
+in1=/home/gdbecknelllab/xxw004/Projects/Ashley/Upk1bProject/Datasets/230308_Becknell_GSL-AJ-3279
+in2=/home/gdbecknelllab/xxw004/Projects/Ashley/Upk1bProject/Datasets/230308_Becknell_GSL-AJ-3279_run2
+
+in=/home/gdbecknelllab/xxw004/Projects/Ashley/Upk1bProject/Datasets/SampleCombined
+out=/home/gdbecknelllab/xxw004/Projects/Ashley/Upk1bProject/Datasets/SampleCombined
+
+
+### set up the sample information
+
+id=Sample_9_GSL-AJ-3279
+Substr=_S9
+
+
+# Generate the inputs of reads substring
+
+# the first run in /home/gdbecknelllab/xxw004/Projects/Ashley/Upk1bProject/Datasets/230308_Becknell_GSL-AJ-3279/
+# the first part in the first run
+read1In1=_L001_R1_001.fastq
+read2In1=_L001_R2_001.fastq
+
+# second part in the first run
+read1In2=_L002_R1_001.fastq
+read2In2=_L002_R2_001.fastq
+
+# the second run in /home/gdbecknelllab/xxw004/Projects/Ashley/Upk1bProject/Datasets/230308_Becknell_GSL-AJ-3279
+# they generated another run that contains the 
+read1In3=_L002_R1_001.fastq
+read2In3=_L002_R2_001.fastq
+
+# we combined the reads for each run and sub-runs
+read1=_Combined_R1_001.fastq
+read2=_Combined_R2_001.fastq
+
+cut1=_Combined_R1_001_val_1.fq
+cut2=_Combined_R2_001_val_2.fq
+
+date
+
+echo "start anlyzing the sequence ...."
+
+# unzip the first run for read1 read2 in In1 and In2
+# gunzip $in1/${id}/${id}${Substr}${read1In1}.gz
+# gunzip $in1/${id}/${id}${Substr}${read2In1}.gz
+#
+# gunzip $in1/${id}/${id}${Substr}${read1In2}.gz
+# gunzip $in1/${id}/${id}${Substr}${read2In2}.gz
+#
+# gunzip $in2/${id}/${id}${Substr}${read1In2}.gz
+# gunzip $in2/${id}/${id}${Substr}${read2In2}.gz
+
+
+# cat the zipped files
+#
+# cat $in1/${id}/${id}${Substr}${read1In1}.gz $in1/${id}/${id}${Substr}${read1In2}.gz $in2/${id}/${id}${Substr}${read1In2}.gz >${in}/${id}${read1}.gz
+# cat $in1/${id}/${id}${Substr}${read2In1}.gz $in1/${id}/${id}${Substr}${read2In2}.gz $in2/${id}/${id}${Substr}${read2In2}.gz >${in}/${id}${read2}.gz
+#
+# # then unzip the files
+# gunzip ${in}/${id}${read1}.gz
+# gunzip ${in}/${id}${read2}.gz
+#
+# # Evaluate the raw reads
+#
+# fastqc $in/${id}${read1}
+# fastqc $in/${id}${read2}
+#
+# # meaure the raw reads number:
+# wc -l $in/${id}${read1} |awk '{ave=2*$1/4;print "RawReadnumber\t"ave}' >${in}/${id}_FinalReadStat.txt
+#
+# # remove the bad sequences
+# echo "start remove the bad sequence"
+# trim_galore --paired --retain_unpaired  --dont_gzip -o $out --fastqc_args "-d ~/scratch" $in/${id}${read1}   $in/${id}${read2}
+# wc -l $in/${id}${cut1} |awk '{ave=2*$1/4;print "HighQualityReadnumber\t"ave}' >>${in}/${id}_FinalReadStat.txt
+#
+# #trim_galore -o $out $in/${id}${read}
+#
+# echo "trim_galore finished"
+
+## Run the tophat
+echo "tophat started"
+
+tophat  -p 12  -G $gtf -o $out/${id} $mm10 $in/${id}${cut1} $in/${id}${cut2}   2>$out/${id}.tophat.err
+
+
+echo "tophat finished"
+
+date
+### run cufflinks
+
+echo "tophat started"
+cufflinks -o $out/${id} -G $gtf $out/${id}/accepted_hits.bam
+
+echo "cufflinks finished"
+date
+
+# check the mapping rate:
+grep "concordant pair alignment rate" $out/${id}/align_summary.txt | perl -ne '{my $mappped= (split/\s+/,$_)[0]; print "MappingRate\t$mapped\n"}' >>${in}/${id}_FinalReadStat.txt
+
+### only retain the the uniqu maping
+echo "samtools started"
+samtools sort -n  $out/$id/accepted_hits.bam  -o $out/$id/accepted.sort_n.bam
+samtools view -b -F 1548 -q 30 $out/$id/accepted.sort_n.bam  -o $out/${id}.srt.filtered.bam
+echo "samtools finished"
+date
+echo "count by htseq-count"
+echo  "ID       $id" >$out/${id}_ssout
+htseq-count -f bam -q -r name  -m union -s reverse  $out/${id}.srt.filtered.bam  $gtf >>$out/$id\_ssout
+echo  "ID       $id" >$out/${id}_count
+htseq-count -f bam -q -r name  -m union -s no  $out/${id}.srt.filtered.bam  $gtf >>$out/$id\_count
+
+
+### finished
+
+echo "congratulation your task has done to get the read count for each gene!"
+
+
+echo "Remove redundant files and gzip the fastq file...."
+
+gzip $in/*.fq
+gzip $in/*.fastq
+
+
+
